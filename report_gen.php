9de81c588a911481de325787e2feb13297a1b7aa
Rohan <rohansheth17@gmail.com>
2017-04-02 21:31:32 +0530

    change info in table format

--- report_gen.php
+++ report_gen.php
@@ -8,17 +8,67 @@
 include("config.php");
 include("nav_bar.php");
 
+/*function getcsv($fields)
+{
+    $separator = '';
+    foreach ($fields as $field) {
+        if (preg_match('/\\r|\\n|,|"/', $field)) {
+            $field = '"' . str_replace('"', '""', $field) . '"';
+        }
+        echo $separator . $field;
+        $separator = ',';
+    }
+    echo "\r\n";
+    return;
+}*/
+
 if(isset($_POST['start_sr']) && isset($_POST['end_sr']))
 {
     $start=$_POST['start_sr'];
     $end=$_POST['end_sr'];
-    $query="select *from report_dtb into outfile '/Downloads/report.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' where sr_no BETWEEN '$start' and '$end'";
-    $result=mysqli_query($db_var,$query);
+   // $query="select * from report_dtb into outfile 'C:\Users\Rohan\Desktop\report.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' where sr_no BETWEEN '$start' and '$end'";
+  //  $result=mysqli_query($db_var,$query);
 
-    echo"<table>
 
-        <>
-";
+    $output = fopen("php://output", "w");
+    fputcsv($output, array('Id', 'UID', 'Sr_no', 'Name', 'Age', 'Sex', 'Address', 'Period', 'From_stn', 'To_stn', 'Class', 'Issued_date', 'DOB'));
+    $query = "SELECT * from report_dtb where Sr_no BETWEEN $start and $end";
+
+    $result = mysqli_query($db_var,$query);
+   while($row = mysqli_fetch_assoc($result))
+    {
+        fputcsv($output, $row);
+    }
+    fclose($output);
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=report.csv');
+
+  /*  $row = mysqli_fetch_assoc($result);
+    if ($row) {
+        getcsv(array_keys($row));
+    }*/
+
+    /*
+     * output data rows (if atleast one row exists)
+     */
+
+ /*   while ($row) {
+        getcsv($row);
+        $row = mysqli_fetch_assoc($result);
+    }*/
+
+    /*
+     * echo the input array as csv data maintaining consistency with most CSV implementations
+     * - uses double-quotes as enclosure when necessary
+     * - uses double double-quotes to escape double-quotes
+     * - uses CRLF as a line separator
+     */
+
+
+    /*   echo"<table>
+
+           <>
+   ";*/
 
     /*SELECT order_id,product_name,qty FROM orders
 INTO OUTFILE '/tmp/orders.csv'
@@ -38,14 +88,19 @@ LINES TERMINATED BY '\n'*/
     <h4> Enter details for Report Generations</h4>
     <form role="form" method="POST" action="report_gen.php">
         <div class="form-group">
+            <div class="col-xs-4">
             <label class="control-label" ">Start SR Number</label>
-            <input class="form-control" name="start_sr" required="required" placeholder="Enter Start SR Number">
+            <input class="form-control" id="ex3" name="start_sr" required="required" placeholder="Enter Start SR Number">
+            </div>
         </div>
+        <br><br><br><br>
         <div class="form-group">
+            <div class="col-xs-4">
             <label class="control-label" ">End SR Number</label>
-            <input class="form-control" name="end_sr" required="required" placeholder="Enter End SR Number">
-
+            <input class="form-control" id="ex3" name="end_sr" required="required" placeholder="Enter End SR Number">
+            </div>
         </div>
+        <br><br><br><br>
 
         <button type="submit" class="btn btn-large btn-success">Generate Report</button>
     </form>
