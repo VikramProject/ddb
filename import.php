65892f277325bd12711e53285b5b50affae82a5d
zainahmeds <zainahmeds123@gmail.com>
2017-10-03 21:53:05 +0530

    fix multiple sheets

--- import.php
+++ import.php
@@ -1,5 +1,7 @@
 <?php
 include('config.php');
+include("admin_dashboard.php");
+
 if (isset($_POST["Import"])) {
     require 'Classes/PHPExcel/IOFactory.php';
 
@@ -22,56 +24,58 @@ if (isset($_POST["Import"])) {
         }
 
 //  Get worksheet dimensions
-        try {
-            $sheet = $objPHPExcel->getSheet(8);
-            $highestRow = $sheet->getHighestRow();
-            $highestColumn = $sheet->getHighestColumn();
-        } catch (Exception $e) {
-            die('Error loading file "' . pathinfo($inputfilename, PATHINFO_BASENAME) . '": ' . $e->getMessage());
-        }
-//  Loop through each row of the worksheet in turn
-        $remove[] = "'";
-        $remove[] = '"';
-        for ($row = 1; $row <= $highestRow; $row++) {
-            //  Read a row of data into an array
-            $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
-            //echo date("Y-m-d",strtotime(str_replace("/","-",$rowData[0][10])))."\n";
-            //  Insert row data array into your database of choice here
-            $sql = "INSERT INTO student (UID,Password,Name,Email,Sex,DOB,Address,Category)
-			VALUES ('" . $rowData[0][2] . "', '" . $password . "', '" . str_replace($remove, "", $rowData[0][5] . " " . $rowData[0][4]) . "', '" . $rowData[0][16] . "', '" . $rowData[0][9] . "', '" . date("Y-m-d",strtotime(str_replace("/","-",$rowData[0][10]))) . "', '" . str_replace($remove, "", $rowData[0][12]) . "', '" . $rowData[0][11] . "')";
+        for($i=0;$i<$objPHPExcel->getSheetCount();$i++) {
 
-            if (mysqli_query($db_var, $sql)) {
-                $exceldata[] = $rowData[0];
-            } else {
-                $errorexceldata[] = $rowData[0];
-                $whatError[] = substr(mysqli_error($db_var), 0, 9);
+
+            try {
+                $sheet = $objPHPExcel->getSheet($i);
+                $highestRow = $sheet->getHighestRow();
+                $highestColumn = $sheet->getHighestColumn();
+            } catch (Exception $e) {
+                die('Error loading file "' . pathinfo($inputfilename, PATHINFO_BASENAME) . '": ' . $e->getMessage());
             }
-        }
-//Cleanup
-        $sql = "DELETE FROM student WHERE UID = 0";
-        mysqli_query($db_var, $sql);
-// Print excel data
-        include ("admin_dashboard.php");
-        echo "Successful Data Entry:- \n<table>";
-        foreach ($exceldata as $index => $excelraw) {
-            echo "<tr>";
-            echo "<td>" . $excelraw[1] . "</td>";
-            echo "<td>" . $excelraw[2] . "</td>";
-            echo "<td>" . $excelraw[5] . $excelraw[4] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-        $i = 0;
-        echo "ERRORS in Data Entry:- \n<table>";
-        foreach ($errorexceldata as $index => $excelraw) {
-            echo "<tr>";
-            echo "<td>" . $excelraw[1] . "</td>";
-            echo "<td>" . $excelraw[2] . "</td>";
-            echo "<td>" . $whatError[$i++] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
+//  Loop through each row of the worksheet in turn
+            $remove[] = "'";
+            $remove[] = '"';
+            for ($row = 1; $row <= $highestRow; $row++) {
+                //  Read a row of data into an array
+                $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
+                //echo date("Y-m-d",strtotime(str_replace("/","-",$rowData[0][10])))."\n";
+                //  Insert row data array into your database of choice here
+                $sql = "INSERT INTO student (UID,Password,Name,Email,Sex,DOB,Address,Category)
+			VALUES ('" . $rowData[0][2] . "', '" . $password . "', '" . str_replace($remove, "", $rowData[0][5] . " " . $rowData[0][4]) . "', '" . $rowData[0][16] . "', '" . $rowData[0][9] . "', '" . date("Y-m-d", strtotime(str_replace("/", "-", $rowData[0][10]))) . "', '" . str_replace($remove, "", $rowData[0][12]) . "', '" . $rowData[0][11] . "')";
 
+                if (mysqli_query($db_var, $sql)) {
+                    $exceldata[] = $rowData[0];
+                } else {
+                    $errorexceldata[] = $rowData[0];
+                    $whatError[] = substr(mysqli_error($db_var), 0, 9);
+                }
+            }
+//Cleanup
+            $sql = "DELETE FROM student WHERE UID = 0";
+            mysqli_query($db_var, $sql);
+// Print excel data
+            echo "Successful Data Entry:- \n<table>";
+            foreach ($exceldata as $index => $excelraw) {
+                echo "<tr>";
+                echo "<td>" . $excelraw[1] . "</td>";
+                echo "<td>" . $excelraw[2] . "</td>";
+                echo "<td>" . $excelraw[5] . $excelraw[4] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            $i = 0;
+            echo "ERRORS in Data Entry:- \n<table>";
+            foreach ($errorexceldata as $index => $excelraw) {
+                echo "<tr>";
+                echo "<td>" . $excelraw[1] . "</td>";
+                echo "<td>" . $excelraw[2] . "</td>";
+                echo "<td>" . $whatError[$i++] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
         mysqli_close($db_var);
     }
     else
